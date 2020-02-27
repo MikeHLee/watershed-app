3066f2a34f1e7865bdb6c48d17b576f1538efcce
Ubuntu <ubuntu@ip-172-31-88-154.ec2.internal>
2020-02-27 01:14:57 +0000

    added sending data from device, display in dashboard

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 // us to parse data from incoming requests. 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+var temp = 0;
 
 // This route will respond with the public, static, landing
 // page for Oasis at some point. 
@@ -13,12 +14,17 @@ app.get('/', (req, res) => res.send('hello world!'));
 
 // This page will serve the dashboard that users can actually use
 // to control the unit. 
-app.get('/dashboard', (req, res) => res.send('dashboard'));
+app.get('/dashboard', (req, res) => {
+	//res.send('dashboard')
+        let data = temp;
+        res.send(`You sent: ${data}`);
+});
 
 // This route is for the data coming from the unit. 
 app.post('/api/heartbeat', (req, res) => {
 	let data = req.body;
 	console.log(data);
+	temp = JSON.stringify(data);
 	res.send(`You sent: ${JSON.stringify(data)}`);
 });
 
