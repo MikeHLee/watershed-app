aa97d3fbe9a7f7e6d256042714571d7e3b29659c
Ubuntu <ubuntu@ip-172-31-88-154.ec2.internal>
2020-02-27 02:19:01 +0000

    Sending data to pi

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = 3000;
 // us to parse data from incoming requests. 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+//create placeholder global variables
 var temp = 0;
+
+
+//load desired setpoints
+var tempdes = {'TempDes':76} //evantually going to load this info from the client
 
 // This route will respond with the public, static, landing
 // page for Oasis at some point. 
@@ -15,7 +21,6 @@ app.get('/', (req, res) => res.send('hello world!'));
 // This page will serve the dashboard that users can actually use
 // to control the unit. 
 app.get('/dashboard', (req, res) => {
-	//res.send('dashboard')
         let data = temp;
         res.send(`You sent: ${data}`);
 });
@@ -25,7 +30,13 @@ app.post('/api/heartbeat', (req, res) => {
 	let data = req.body;
 	console.log(data);
 	temp = JSON.stringify(data);
-	res.send(`You sent: ${JSON.stringify(data)}`);
+	res.send(temp);
+});
+
+// This is the route to send the setpoints to the unit.
+app.get('/api/params', (req, res) => {
+	let data = tempdes;
+	res.send(data);
 });
 
 app.listen(port, () => console.log('app is running'));
