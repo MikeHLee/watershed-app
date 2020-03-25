cfae4a8733adca39d6f9b6480253d420399201ef
root <root@ip-172-31-41-117.us-east-2.compute.internal>
2020-03-25 23:40:09 +0000

    realtime data capabilities

---
+++ index4.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// Body Parser is ExpressJS middleware that allows
+// us to parse data from incoming requests. 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//create placeholder global variables
+var temp = 0;
+
+
+//load desired setpoints
+var tempdes = {'TempDes':76} //evantually going to load this info from the client
+
+// This route will respond with the public, static, landing
+// page for Oasis at some point. 
+app.get('/', (req, res) =>{ 
+	res.send('server running');
+	res.send('--------------');
+	res.send('check out: displayData2/')
+	
+});
+
+
+
+//this is for saving the data
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+  path: 'data2.csv',
+  header: [
+	  {id: 'temperature', title: 'Temp'},
+	  {id: 'idealTemp'  , title: 'IdealTemp'}
+  ]
+});
+
+
+
+var visData = [];
+// This route is for the data coming from the unit. 
+app.post('/api/heartbeat', (req, res) => {
+	let data = req.body;
+	console.log(data);
+	temp = JSON.stringify(data);
+	res.send(temp);
+
+	//add data for csv
+	visData.push({temperature: data['heat'],
+			idealTemp: tempdes['TempDes']});
+
+
+	//real time data
+	var today = new Date();
+	io.sockets.emit('temp', {date: today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear(), time: (today.getHours())+":"+(today.getMinutes()), temp:data['heat']});
+});
+
+
+
+// for using shell commands, see 
+const { exec } = require("child_process");
+
+// convert lastest data to json -> csv -> html-line chart for display
+var path = require('path');
+app.get('/chart', (req, res) => {
+	//add data to csv
+	csvWriter
+		.writeRecords(visData)
+		.then(()=> console.log('The CSV file was written successfully'));
+	
+	//clear visData
+	visData=[];
+
+	//convert csv to html
+	//https://www.npmjs.com/package/chart-csv
+	//using shell commands
+	//https://stackabuse.com/executing-shell-commands-with-node-js/
+	exec("cat data2.csv | chart-csv > data2.html");
+
+
+	//visualize data
+        res.sendFile(path.join(__dirname +'/data2.html'));
+
+});
+
+
+// This is the route to send the setpoints to the unit.
+app.get('/api/params', (req, res) => {
+	let data = tempdes;
+	res.send(data);
+});
+
+
+//AJS93 -- added functionality to automatically tell when 
+//install: npm install canihazip
+var icanhazip = require('icanhazip');
+icanhazip.IPv4().then(function(myIP) {
+        console.log('------------------')
+	console.log(`Server running at http://${myIP}:${port}/`);
+	console.log('------------------')
+	console.log('dashboard: most resent datum')
+	console.log('tempChart: static temperature line chart chart')
+	console.log('------------------')
+
+
+}).catch(function(e) {
+        console.error(e.message);
+});
+
+
+
+
+
+
+//start the server
+//app.listen(port );
+
+
+//try to make real time stuff here
+// view engine setup
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+var server = app.listen(3000, () => {})
+
+
+var io = require('socket.io')(server); //Bind socket.io to our express server.
+app.use('/realtime', express.static('displayrealtime.html')); //Send index.html page on GET /
+//var today = new Date();
+//io.sockets.emit('temp', {date: today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear(), time: (today.getHours())+":"+(today.getMinutes()), temp:10});
+io.on('connection', (socket) => {
+    console.log("Someone connected."); //show a log as a new client connects.
+})
+
+
+
+
+
+; //why is this here.. it needs to be tho...
