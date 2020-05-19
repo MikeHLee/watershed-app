e76653896190777d7817e3697f51214be44b0d12
Ubuntu <ubuntu@ip-172-31-91-56.ec2.internal>
2020-05-19 22:35:22 +0000

    Turned image right-side-up

--- imageprocessor.py
+++ imageprocessor.py
@@ -3,12 +3,41 @@ import cv2
 im = cv2.imread('/home/ubuntu/watershed-app/images/pic.jpg')
 
 print(im)
-
 print(type(im))
 # <class 'numpy.ndarray'>
-
 print(im.shape)
 # (225, 400, 3)
-
 print(im.dtype)
 # uint8
+
+def rotate_image(mat, angle):
+    """
+    Rotates an image (angle in degrees) and expands image to avoid cropping
+    """
+
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0])
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+edited_image = rotate_image(im, 180)
+
+# save image
+status = cv2.imwrite('/home/ubuntu/watershed-app/images/pic.jpg', edited_image)
+print("Image written to file-system: ",status)
